eca0af2eddc61687db0807534429337c67443429
UtopiaBeam <utopiabeam@gmail.com>
2020-10-17 15:02:16 +0700

    ✨ Add config module

---
+++ backend/src/config/config.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ConfigService {
+    private getEnv(name: string): string {
+        const env = process.env[name];
+        if (name) {
+            return name;
+        }
+        throw new Error(`${name} is undefined`);
+    }
+
+    get googleProjectId(): string {
+        return this.getEnv('GOOGLE_PROJECT_ID');
+    }
+
+    get gCloudServiceKey() {
+        const key = this.getEnv('GCLOUD_SERVICE_KEY');
+        return JSON.parse(key);
+    }
+}
