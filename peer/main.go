82e7b4e4c82955b148e31819b5ca6250934cb747
Niti Assavaplakorn <nitikub@gmail.com>
2020-11-28 17:54:33 +0700

    remove hardcode port

--- peer/main.go
+++ peer/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"peer/routers"
+	"peer/services"
 	"peer/services/consul"
 )
 
 func main() {
 	consul.RegisterServiceWithConsul()
 	r := routers.SetupRouter()
-	r.Run(":9000")
+	r.Run(services.Port())
 }
